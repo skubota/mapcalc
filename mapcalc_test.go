3f7d2d3b51284068d36e0d57958309e70652cd78
skubota <skubota@xn--y8jmg4a9b7g.jp>
2020-07-30 11:56:24 +0900

    fix

--- mapcalc_test.go
+++ mapcalc_test.go
@@ -5,22 +5,22 @@ import (
 	"testing"
 )
 
-func Testmap6to4(t *testing.T) {
+func TestMap6to4(t *testing.T) {
 	r := Rules{
 		Maprule{
 			Ipv6:   "2001:db8::/32",
 			Ipv4:   "10.0.0.0/16",
-			Ea_len: 24,
+			Ea_length: 24,
 		},
 		Maprule{
 			Ipv6:   "2001:db9::/32",
 			Ipv4:   "10.1.0.0/16",
-			Ea_len: 24,
+			Ea_length: 24,
 		},
 		Maprule{
 			Ipv6:   "2001:db10::/32",
 			Ipv4:   "10.2.0.0/16",
-			Ea_len: 24,
+			Ea_length: 24,
 		},
 	}
 	addr, port := Map6to4("2001:db8:0101:ab00::1", r)
